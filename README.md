861bb12af37c28d528c90d98c2699d8caf273c37
Jell <jean-louis@jawaninja.com>
2021-03-14 09:19:16 +0100

    Nicer examples

--- README.md
+++ README.md
@@ -25,24 +25,24 @@ use std::{io, str::from_utf8};
 
 fn main() -> io::Result<()> {
     let nc = nats::connect("nats://127.0.0.1:4222")?;
-    let client = stan::connect(nc, "test-cluster", "rust-client-1")?;
+    let sc = stan::connect(nc, "test-cluster", "rust-client-1")?;
 
-    println!("Connected: {:?}", client);
+    sc.publish("foo", "hello from rust 1")?;
 
-    println!("sending message 1");
-    client.publish("foo", "hello from rust 1")?;
+    let sub = sc
+        .subscribe("foo", Some("foo-2"), None)?
+        .with_handler(|msg| {
+            println!("{:?}", from_utf8(&msg.data));
+            Ok(())
+        });
 
-    let sub_handler = client.subscribe("foo", Some("foo-2"), None)?.with_handler(|msg| {
-        println!("{:?}", from_utf8(&msg.data));
-        Ok(())
-    });
+    sc.publish("foo", "hello from rust 2")?;
+    sc.publish("foo", "hello from rust 3")?;
 
-    client.publish("foo", "hello from rust 2")?;
-    client.publish("foo", "hello from rust 3")?;
+    sub.unsubscribe()?;
 
-    sub_handler.unsubscribe()?;
+    sc.publish("foo", "hello from rust 4")?;
 
-    client.publish("foo", "hello from rust 4")?;
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,23 @@ use std::{io, str::from_utf8, thread, time};
 
 fn main() -> io::Result<()> {
     let nc = nats::connect("nats://127.0.0.1:4222")?;
-    let client = stan::connect(nc, "test-cluster", "rust-client-1")?;
+    let sc = stan::connect(nc, "test-cluster", "rust-client-1")?;
 
-    println!("Connected: {:?}", client);
+    sc.publish("foo", "hello from rust 1")?;
 
-    println!("sending message 1");
-    client.publish("foo", "hello from rust 1")?;
+    let sub = sc
+        .subscribe("foo", Some("foo-2"), None)?
+        .with_handler(|msg| {
+            println!("{:?}", from_utf8(&msg.data));
+            Ok(())
+        });
 
-    let sub_handler = client.subscribe("foo", Some("foo-2"), None)?.with_handler(|msg| {
-        println!("{:?}", from_utf8(&msg.data));
-        Ok(())
-    });
+    sc.publish("foo", "hello from rust 2")?;
+    sc.publish("foo", "hello from rust 3")?;
 
-    client.publish("foo", "hello from rust 2")?;
-    println!("sending message 2");
-    client.publish("foo", "hello from rust 3")?;
-
-    sub_handler.unsubscribe()?;
+    sub.unsubscribe()?;
 
     thread::sleep(time::Duration::from_secs(1));
-    client.publish("foo", "hello from rust 4")?;
+    sc.publish("foo", "hello from rust 4")?;
     Ok(())
 }
