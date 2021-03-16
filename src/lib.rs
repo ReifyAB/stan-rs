231cda9718dc8792ec74c53905739b446ccab6ad
Jell <jean-louis@jawaninja.com>
2021-03-16 19:16:26 +0100

    Document subscription

--- src/lib.rs
+++ src/lib.rs
@@ -97,6 +97,51 @@ fn process_ack(msg: nats::Message) -> io::Result<()> {
 }
 
 #[derive(Clone)]
+/// NATS Streaming subscription
+///
+///# Example:
+///```
+/// use nats;
+/// use std::{io, str::from_utf8, time};
+///
+///  fn main() -> io::Result<()> {
+///      let nc = nats::connect("nats://127.0.0.1:4222")?;
+///      let sc = stan::connect(nc, "test-cluster", "rust-client-1")?;
+///#
+///#     sc.publish("foo", "hello from rust 1")?;
+///#
+///      let sub1 = sc
+///          .subscribe("foo", Default::default())?
+///          .with_handler(|msg| {
+///              println!("sub1 got {:?}", from_utf8(&msg.data));
+///              msg.ack()?;
+///              println!("manually acked!");
+///              Ok(())
+///          });
+///
+///      sc.subscribe("foo", Default::default())?
+///          .with_handler(|msg| {
+///              println!("sub 2 got {:?}", from_utf8(&msg.data));
+///              Ok(())
+///          });
+///
+///      for msg in sc.subscribe("foo", Default::default())?.messages() {
+///          println!("sub 3 got {:?}", from_utf8(&msg.data));
+///          msg.ack()?;
+///#        break;
+///      }
+///
+///      for msg in sc
+///          .subscribe("foo", Default::default())?
+///          .timeout_iter(time::Duration::from_secs(1))
+///      {
+///          println!("sub 4 got {:?}", from_utf8(&msg.data));
+///          msg.ack()?;
+///#        break;
+///      }
+///
+///      Ok(())
+///  }
 pub struct Subscription {
     nats_subscription: nats::Subscription,
     inner: Arc<InnerSub>,
