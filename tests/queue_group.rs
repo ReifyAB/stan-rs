518a3b163e80bfcecc3fe5e1b32b189e2e6c63c9
Jell <jean-louis@jawaninja.com>
2021-03-21 11:03:17 +0100

    Cleanup test

--- tests/queue_group.rs
+++ tests/queue_group.rs
@@ -3,6 +3,38 @@ mod utils;
 use logtest::Logger;
 
 #[test]
+fn test_without_queue_group() -> io::Result<()> {
+    let server = utils::server()?;
+    let nats_url = &format!("localhost:{}", server.port);
+    let nc = nats::connect(nats_url)?;
+    let sc = stan::connect(nc, "test-cluster", "rust-client-1")?;
+    let mut logger = Logger::start();
+
+    for _ in 0..4 {
+        // fork 4 subscriptions without group queue
+        sc.subscribe(
+            "foo",
+            stan::SubscriptionConfig {
+                queue_group: None,
+                ..Default::default()
+            },
+        )?.with_handler(|msg| {
+            log::info!("received: {}", from_utf8(&msg.data).unwrap());
+            Ok(())
+        });
+    }
+
+    sc.publish("foo", "hello from rust 1")?;
+
+    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
+    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
+    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
+    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
+
+    Ok(())
+}
+
+#[test]
 fn test_with_queue_group() -> io::Result<()> {
     let server = utils::server()?;
     let nats_url = &format!("localhost:{}", server.port);
@@ -34,37 +66,5 @@ fn test_with_queue_group() -> io::Result<()> {
     assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 2");
     assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 3");
     assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 4");
-    Ok(())
-}
-
-#[test]
-fn test_without_queue_group() -> io::Result<()> {
-    let server = utils::server()?;
-    let nats_url = &format!("localhost:{}", server.port);
-    let nc = nats::connect(nats_url)?;
-    let sc = stan::connect(nc, "test-cluster", "rust-client-1")?;
-    let mut logger = Logger::start();
-
-    for _ in 0..4 {
-        // fork 4 subscriptions on the same queue group
-        sc.subscribe(
-            "foo",
-            stan::SubscriptionConfig {
-                queue_group: None,
-                ..Default::default()
-            },
-        )?.with_handler(|msg| {
-            log::info!("received: {}", from_utf8(&msg.data).unwrap());
-            Ok(())
-        });
-    }
-
-    sc.publish("foo", "hello from rust 1")?;
-
-    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
-    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
-    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
-    assert_eq!(logger.pop().unwrap().args(), "received: hello from rust 1");
-
     Ok(())
 }
